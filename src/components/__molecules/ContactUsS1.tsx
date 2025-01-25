aa9dc1f8e13e524fa4d0b1eb0f1bd5f024abf5d6
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-25 17:22:48 +0400

    added responsive.

--- src/components/__molecules/ContactUsS1.tsx
+++ src/components/__molecules/ContactUsS1.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function ContactUsS1() {
   return (
     <>
-      <div className="max-w-[1120px] m-auto">
-        <h2 className="font-medium text-[54px] leading-[58px] text-[#141718]">
+      <div className="max-w-[560px] sm:max-w-[1120px] m-auto ">
+        <h2 className="font-medium text-[54px] leading-[58px] text-[#141718] ">
           We believe in sustainable decor. We’re passionate about life at home.
         </h2>
-        <p className="font-normal text-[16px] leading-[26px] text-[#141718] mt-[24px]">
+        <p className="font-normal text-[16px] leading-[26px] text-[#141718] mt-[24px] ">
           Our features timeless furniture, with natural fabrics, curved lines,
           plenty of mirrors and classic design, which can be incorporated into
           any decor project. The pieces enchant for their sobriety, to last for
--- src/components/__molecules/Header.tsx
+++ src/components/__molecules/Header.tsx
@@ -20,7 +20,7 @@ import { useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+import { AnimatePresence, motion } from "framer-motion";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const PathName = usePathname();
@@ -46,111 +46,120 @@ const Header = () => {
           onClick={toggleMenu}
           className="cursor-pointer"
         />
-        {isOpen && (
-          <div className="relative z-10">
-            <div
-              className={`fixed top-0 left-0 h-screen bg-white shadow-lg transform ${
-                isOpen ? "translate-x-0" : "-translate-x-full"
-              } transition-transform duration-500 ease-in-out w-[343px]`}
-            >
-              <div className="flex">
-                <button
-                  className="absolute top-4 right-4 text-3xl text-black"
-                  onClick={toggleMenu}
-                >
-                  &times;
-                </button>
-                <div className="absolute w-[70px] h-[40px] top-6 left-6">
-                  <Image
-                    src={BlackLogo}
-                    layout="fill"
-                    objectFit="contain"
-                    alt="logo"
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div className="relative z-10">
+              <motion.div
+                className={`fixed top-0 left-0 h-screen bg-white shadow-lg transform ${
+                  isOpen ? "translate-x-0" : "-translate-x-full"
+                }  w-[343px]`}
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                viewport={{
+                  once: true,
+                }}
+              >
+                <div className="flex">
+                  <button
+                    className="absolute top-4 right-4 text-3xl text-black"
+                    onClick={toggleMenu}
+                  >
+                    &times;
+                  </button>
+                  <div className="absolute w-[70px] h-[40px] top-6 left-6">
+                    <Image
+                      src={BlackLogo}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="logo"
+                    />
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    className="absolute border-[1px] border-black top-[64px] ml-6 max-w-[294px] w-full rounded-[6px] pl-2 h-[46px]"
                   />
                 </div>
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="absolute border-[1px] border-black top-[64px] ml-6 max-w-[294px] w-full rounded-[6px] pl-2 h-[46px]"
-                />
-              </div>
-              <div className="p-6 absolute top-[126px]">
-                <nav className="w-[294px]">
-                  <ul className="flex gap-4 flex-col">
-                    {navLinks.map((item) => {
-                      const isActive = PathName.startsWith(item.link);
-                      return (
-                        <li
-                          key={item.link}
-                          className="border-b-[1px] h-[40px] border-[#E8ECEF]"
-                        >
-                          <span
-                            className={`font-publicSans font-bold leading-[24px] hover:text-black group-hover:text-black duration-1000 ${
-                              isActive
-                                ? "text-[#201F24] group-hover:text-black"
-                                : "text-[#141718]"
-                            } text-[14px]`}
+                <div className="p-6 absolute top-[126px]">
+                  <nav className="w-[294px]">
+                    <ul className="flex gap-4 flex-col">
+                      {navLinks.map((item) => {
+                        const isActive = PathName.startsWith(item.link);
+                        return (
+                          <li
+                            key={item.link}
+                            className="border-b-[1px] h-[40px] border-[#E8ECEF]"
                           >
-                            <Link href={item.link}>{item.title}</Link>
-                          </span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </nav>
-              </div>
-              <div className="absolute w-[295px] h-[210px] top-[500px] p-[24px] m-auto">
-                <div className="flex justify-between border-b-[1px] border-[#E8ECEF] h-[40px] items-center">
-                  <h6>Cart</h6>
-                  <Image
-                    src={cartIcon}
-                    width={24}
-                    height={24}
-                    alt="cartIcon"
-                    className="cursor-pointer"
-                  />
+                            <span
+                              className={`font-publicSans font-bold leading-[24px] hover:text-black group-hover:text-black duration-1000 ${
+                                isActive
+                                  ? "text-[#201F24] group-hover:text-black"
+                                  : "text-[#141718]"
+                              } text-[14px]`}
+                            >
+                              <Link href={item.link}>{item.title}</Link>
+                            </span>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </nav>
                 </div>
-                <div className="flex justify-between border-b-[1px] border-[#E8ECEF] h-[40px] items-center">
-                  <h6>Wishlist</h6>
-                  <Image
-                    src={cartIcon}
-                    width={24}
-                    height={24}
-                    alt="cartIcon"
-                    className="cursor-pointer"
-                  />
+                <div className="absolute w-[295px] h-[210px] top-[500px] p-[24px] m-auto">
+                  <div className="flex justify-between border-b-[1px] border-[#E8ECEF] h-[40px] items-center">
+                    <h6>Cart</h6>
+                    <Image
+                      src={cartIcon}
+                      width={24}
+                      height={24}
+                      alt="cartIcon"
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <div className="flex justify-between border-b-[1px] border-[#E8ECEF] h-[40px] items-center">
+                    <h6>Wishlist</h6>
+                    <Image
+                      src={cartIcon}
+                      width={24}
+                      height={24}
+                      alt="cartIcon"
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="bg-black w-full text-white py-2 px-4 rounded mt-5"
+                    onClick={handleClick}
+                  >
+                    Sign In
+                  </button>
+                  <div className="flex mt-5 gap-6">
+                    <Image
+                      src={InstagramIconBlack}
+                      width={24}
+                      height={24}
+                      alt="Instagram Icon"
+                    />
+                    <Image
+                      src={FacebookIconBlack}
+                      width={24}
+                      height={24}
+                      alt="Facebook Icon"
+                    />
+                    <Image
+                      src={youtubeIconBlack}
+                      width={24}
+                      height={24}
+                      alt="YouTube Icon"
+                    />
+                  </div>
                 </div>
-                <button
-                  type="button"
-                  className="bg-black w-full text-white py-2 px-4 rounded mt-5"
-                  onClick={handleClick}
-                >
-                  Sign In
-                </button>
-                <div className="flex mt-5 gap-6">
-                  <Image
-                    src={InstagramIconBlack}
-                    width={24}
-                    height={24}
-                    alt="Instagram Icon"
-                  />
-                  <Image
-                    src={FacebookIconBlack}
-                    width={24}
-                    height={24}
-                    alt="Facebook Icon"
-                  />
-                  <Image
-                    src={youtubeIconBlack}
-                    width={24}
-                    height={24}
-                    alt="YouTube Icon"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
 
         <Image
           src={BlackLogo}
@@ -188,6 +197,7 @@ const Header = () => {
           height={24}
           alt="User"
           className="cursor-pointer sm:flex hidden"
+          onClick={handleClick}
         />
         <Image
           src={cartIcon}
