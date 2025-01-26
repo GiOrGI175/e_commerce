12175940600361ad84f5b49ae5227abeaf1e1845
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-27 02:19:29 +0400

    feature/f

--- src/components/__atoms/productCard.tsx
+++ src/components/__atoms/productCard.tsx
@@ -1,16 +1,28 @@
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function ProductCard({
   onAddToCart,
+  setMyObject,
   id,
+  name,
+  price,
+  oldPrice,
+  discount,
+  rating,
+  isNew,
+  image,
+  description,
+  images,
+  stars,
+  category,
+  wishlist,
+  quantity,
+  SKU,
+  chooseColor,
   layout,
   ProductImage,
-  name,
-  discount,
-  price,
-  isNew,
-  description,
 }: any) {
   const [showAddToCart, setShowAddToCart] = useState(false);
 
@@ -27,45 +39,64 @@ export default function ProductCard({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className={`${
-        layout === "Sort3"
-          ? "max-w-[548px] h-fit flex flex-col sm:flex-row gap-x-[24px] items-center"
-          : "h-fit border-gray-200 rounded-lg overflow-hidden sm:max-w-[262px] max-w-[152px]"
+        layout === 'Sort3'
+          ? 'max-w-[548px] h-fit flex flex-col sm:flex-row gap-x-[24px] items-center'
+          : 'h-fit border-gray-200 rounded-lg overflow-hidden sm:max-w-[262px] max-w-[152px]'
       }`}
     >
       {/* Product Image */}
-      <div className="relative w-[240px] ">
+      <div className='relative w-[240px] '>
         <Image
-          className="w-full object-cover"
+          className='w-full object-cover'
           src={ProductImage}
-          alt="Product Image"
+          alt='Product Image'
           width={262}
           height={349}
         />
         <div
-          className={`${layout === "Sort3" && "hidden"} absolute top-2 left-2`}
+          className={`${layout === 'Sort3' && 'hidden'} absolute top-2 left-2`}
         >
-          <div className="flex flex-col gap-2">
-            <div className="bg-black text-white text-sm font-bold px-[14px] py-[4px] rounded-[4px]">
-              {isNew && "NEW"}
+          <div className='flex flex-col gap-2'>
+            <div className='bg-black text-white text-sm font-bold px-[14px] py-[4px] rounded-[4px]'>
+              {isNew && 'NEW'}
             </div>
-            <div className="bg-[#38CB89] text-white text-sm font-bold px-[14px] py-[4px] rounded-[4px]">
+            <div className='bg-[#38CB89] text-white text-sm font-bold px-[14px] py-[4px] rounded-[4px]'>
               {discount}
             </div>
           </div>
         </div>
         {showAddToCart && (
-          <div className="flex justify-center">
+          <div className='flex justify-center'>
             <button
-              onClick={() =>
+              onClick={() => {
                 onAddToCart({
                   id,
                   name,
                   price,
-                })
-              }
-              className="absolute bottom-4 max-w-[200px] w-full bg-black text-white rounded-md py-[9px]"
+                });
+
+                setMyObject({
+                  id,
+                  name,
+                  price,
+                  oldPrice,
+                  discount,
+                  rating,
+                  isNew,
+                  image,
+                  description,
+                  images,
+                  stars,
+                  category,
+                  wishlist,
+                  quantity,
+                  SKU,
+                  chooseColor,
+                });
+              }}
+              className='absolute bottom-4 max-w-[200px] w-full bg-black text-white rounded-md py-[9px]'
             >
-              Add To Cart
+              <Link href={`Shop/${id}`}>Add To Cart</Link>
             </button>
           </div>
         )}
@@ -74,10 +105,10 @@ export default function ProductCard({
       {/* Product Info */}
       <div
         className={`text-[#121212] pt-[12px] ${
-          layout === "Sort3" ? "gap-y-[16px]" : "gap-y-[4px]"
+          layout === 'Sort3' ? 'gap-y-[16px]' : 'gap-y-[4px]'
         } flex flex-col max-w-[282px] w-full`}
       >
-        <div className="flex items-center text-md">
+        <div className='flex items-center text-md'>
           <span>★</span>
           <span>★</span>
           <span>★</span>
@@ -85,13 +116,13 @@ export default function ProductCard({
           <span>★</span>
         </div>
         {/* Product Name */}
-        <p className="font-semibold text-base">{name}</p>
+        <p className='font-semibold text-base'>{name}</p>
         {/* Price */}
-        <span className="font-bold text-xs">{price}</span>
-        {layout === "Sort3" && (
+        <span className='font-bold text-xs'>{price}</span>
+        {layout === 'Sort3' && (
           <>
             <p>{description}</p>
-            <div className="flex flex-col gap-y-[8px]">
+            <div className='flex flex-col gap-y-[8px]'>
               <button
                 onClick={() =>
                   onAddToCart({
@@ -103,11 +134,11 @@ export default function ProductCard({
                     isNew,
                   })
                 }
-                className="bg-black w-full  text-white rounded-md py-[9px]"
+                className='bg-black w-full  text-white rounded-md py-[9px]'
               >
                 Add To Cart
               </button>
-              <button className="py-[9px] font-bold">Wish List</button>
+              <button className='py-[9px] font-bold'>Wish List</button>
             </div>
           </>
         )}
