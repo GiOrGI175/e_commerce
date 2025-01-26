cb4bcb198d7f59d742bbaf01ee9b4fca856a0617
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-26 16:52:13 +0400

    g

---
+++ src/commons/services/productDetails.ts
@@ -0,0 +1,16 @@
+[
+  {
+    id: 1,
+    images: [],
+    price: 199.99,
+    oldPice: 400.0,
+    wishlist: false,
+    quantity: 1,
+    chooseColor: {
+      black: '',
+      brown: '',
+      red: '',
+      white: '',
+    },
+  },
+];
