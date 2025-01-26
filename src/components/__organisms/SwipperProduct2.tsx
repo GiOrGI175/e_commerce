35522eff9c274988586614c4159652664b38b2d2
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-26 21:33:09 +0400

    feature/product_page

---
+++ src/components/__organisms/SwipperProduct2.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import React, { useRef, useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+// import { SwipperSlidesArr } from '@/commons/services/swipperSlides';
+import Image from 'next/image';
+import SwpperArrowR from '../__atoms/SwpperArrowR';
+import SwipperArrowL from '../__atoms/SwipperArrowL';
+import { product } from '@/commons/services/productDetails';
+
+const SwipperProduct2 = () => {
+  const paginationRef = useRef<HTMLDivElement>(null);
+  const [swiperReady, setSwiperReady] = useState(false);
+
+  const [index, setIndex] = useState(0);
+
+  const [swiperInstance, setSwiperInstance] = useState<any>(null);
+
+  useEffect(() => {
+    if (paginationRef.current) {
+      setSwiperReady(true);
+    }
+  }, []);
+
+  const handleSlideChange = (swiper: any) => {
+    setIndex(swiper.realIndex);
+  };
+
+  const handleThumbnailClick = (index: number) => {
+    swiperInstance.slideTo(index);
+  };
+
+  return (
+    <div className='relative max-w-[547px] w-full h-[728px] '>
+      {swiperReady && (
+        <Swiper
+          className='mySwiper3  w-full h-full'
+          slidesPerView={'auto'}
+          spaceBetween={20}
+          pagination={{
+            el: paginationRef.current,
+            clickable: true,
+          }}
+          loop={true}
+          cssMode={true}
+          navigation={{
+            prevEl: '.custom-prev3',
+            nextEl: '.custom-next3',
+          }}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Autoplay]}
+          onSlideChange={handleSlideChange}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          onInit={(swiper) => setSwiperInstance(swiper)}
+        >
+          {product.images.map((item, index) => {
+            return (
+              <SwiperSlide className='swiper-slide3'>
+                <Image src={item} width={1120} height={537} alt='slide' />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      )}
+
+      <div ref={paginationRef} className='custom-pagination3' />
+
+      <button className='custom-prev3'>
+        <SwipperArrowL index={index} />
+      </button>
+      <button className='custom-next3'>
+        <SwpperArrowR index={index} productArr={product.images} />
+      </button>
+
+      <div className='max-w-[547px] w-full mt-[24px] flex justify-between'>
+        {product.images.map((item, idx) => (
+          <div
+            key={idx}
+            className={`thumbnail ${
+              idx === index ? 'border-2 border-[#141718]' : ''
+            }`}
+            onClick={() => handleThumbnailClick(idx)}
+            style={{ cursor: 'pointer' }}
+          >
+            <div className='w-[167px] h-[167px] overflow-hidden'>
+              <Image
+                src={item}
+                width={167}
+                height={167}
+                alt={`thumbnail-${idx}`}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SwipperProduct2;
