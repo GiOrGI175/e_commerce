38a1edea499d6287c9987a3ee0a87bbe3cf86afc
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-27 18:18:09 +0400

    f

--- src/components/__molecules/OrderSummary.tsx
+++ src/components/__molecules/OrderSummary.tsx
@@ -1,7 +1,7 @@
-import { TableImage } from "@/utility/images/ImgExport";
-import Image from "next/image";
-import React from "react";
-import useCartStore from "../__atoms/CartStore";
+import { TableImage } from '@/utility/images/ImgExport';
+import Image from 'next/image';
+import React from 'react';
+import useCartStore from '../__atoms/CartStore';
 
 export default function () {
   const cart = useCartStore((state) => state.cart);
@@ -11,16 +11,16 @@ export default function () {
   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
-    <div className="max-w-[400px] ml-auto mr-auto w-full  p-4 border border-gray-300 rounded-md">
-      <h1 className="text-xl font-bold mb-4">Order Summary</h1>
+    <div className='max-w-[400px] ml-auto mr-auto w-full  p-4 border border-gray-300 rounded-md'>
+      <h1 className='text-xl font-bold mb-4'>Order Summary</h1>
       {cart.map((item) => (
-        <div className="max-w-[376px] w-full h-[144px] flex items-center justify-between border-b-[1px] border-gray-300 ">
-          <div className="flex items-center">
-            <Image src={TableImage} width={80} height={96} alt="table" />
-            <div className="ml-4">
+        <div className='max-w-[376px] w-full h-[144px] flex items-center justify-between border-b-[1px] border-gray-300 '>
+          <div className='flex items-center'>
+            <Image src={TableImage} width={80} height={96} alt='table' />
+            <div className='ml-4'>
               <h5>{item.name}</h5>
-              <h5>{item.color}</h5>
-              <div className="flex justify-around items-center border-[1px] border-black rounded-[4px] max-w-[80px] w-full ">
+              {/* <h5>{item.color}</h5> */}
+              <div className='flex justify-around items-center border-[1px] border-black rounded-[4px] max-w-[80px] w-full '>
                 <button onClick={() => decreaseQuantity(item.id)}>-</button>
                 <h5>{item.quantity}</h5>
                 <button onClick={() => increaseQuantity(item.id)}>-</button>
@@ -46,16 +46,16 @@ export default function () {
         </div>
         <h5>$38.00</h5>
       </div> */}
-      <div className="h-[52px] flex justify-between mt-5">
+      <div className='h-[52px] flex justify-between mt-5'>
         <h5>Shipping</h5>
         <h5>Free</h5>
       </div>
-      <div className="h-[52px] flex justify-between mt-5">
+      <div className='h-[52px] flex justify-between mt-5'>
         <h5>Subtotal</h5>
 
         <h5>${total}</h5>
       </div>
-      <div className="h-[52px] flex justify-between mt-5">
+      <div className='h-[52px] flex justify-between mt-5'>
         <h5>Total</h5>
         <h5>${total}</h5>
       </div>
