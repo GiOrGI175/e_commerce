91b2a7809ef9ad3759f2afdea6a4dada23d259e9
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2025-01-27 16:10:20 +0400

    //

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Header from "@/components/__molecules/Header";
 import Image from "next/image";
 import HomePage from "./(views)/home/page";
 
+
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />;
+    </>
+  );
 }
